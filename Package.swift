3e6b2722ede2a1569e06a8dd4cd89804ba1159ca
howerzhao <275919229@qq.com>
2025-09-24 11:56:19 +0800

    1.0.2

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TestMOBFoundationSPM",
+    platforms: [.iOS(.v12)], // 匹配OC Framework支持的最低版本
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
